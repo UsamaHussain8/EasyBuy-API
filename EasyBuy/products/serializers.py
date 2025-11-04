39efac51b65b3ce8b60ecc34dd14bfeaa5aeb67e
UsamaHussain8 <usamahussain00@gmail.com>
2025-11-04 22:49:54 +0500

    Added read-only field for products and their reviews

--- EasyBuy/products/serializers.py
+++ EasyBuy/products/serializers.py
@@ -13,45 +13,6 @@ class TagSerializer(serializers.ModelSerializer):
             'caption'
         ]
 
-class ProductSerializer(serializers.ModelSerializer):
-    tags = TagSerializer(many=True)
-    seller = StoreUserSerializer(read_only=True)
-    seller_id = serializers.PrimaryKeyRelatedField(
-        queryset = StoreUser.objects.all(),
-        source='seller',
-        write_only=True
-    )
-    class Meta:
-        model = Product
-        fields = [
-            'name',
-            'price',
-            'quantity',
-            'category',
-            'description',
-            'excerpt',
-            'tags',
-            'seller',
-            'seller_id',
-        ]
-
-    @transaction.atomic()
-    def create(self, validated_data):
-        tags = validated_data.pop('tags', [])
-        seller = validated_data.pop('seller', None)
-        #seller, _ = get_object_or_404(StoreUser, pk=seller_id)
-        if not seller:
-            raise serializers.ValidationError(
-                {"seller_id": "Seller not registered or invalid ID."}
-            )
-        product = Product.objects.create(**validated_data, seller=seller)
-        for tag_data in tags:
-            tag_obj, _ = Tag.objects.get_or_create(**tag_data)
-            product.tags.add(tag_obj)
-
-        #product.save()
-        return product
-    
 class ReviewSerializer(serializers.ModelSerializer):
     reviewer = serializers.StringRelatedField(read_only=True)
     product = serializers.StringRelatedField(read_only=True)
@@ -94,3 +55,46 @@ class ReviewSerializer(serializers.ModelSerializer):
         product = self.context['product']
         reviewer = self.context['request'].user.store_user
         return Review.objects.create(product=product, reviewer=reviewer, **validated_data)
+    
+class ProductSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True)
+    seller = StoreUserSerializer(read_only=True)
+    seller_id = serializers.PrimaryKeyRelatedField(
+        queryset = StoreUser.objects.all(),
+        source='seller',
+        write_only=True
+    )
+    reviews = ReviewSerializer(many=True, read_only=True)
+    class Meta:
+        model = Product
+        fields = [
+            'name',
+            'price',
+            'quantity',
+            'category',
+            'description',
+            'excerpt',
+            'tags',
+            'seller',
+            'seller_id',
+            'reviews',
+        ]
+        read_only_fields = ['seller', 'reviews']
+
+    @transaction.atomic()
+    def create(self, validated_data):
+        tags = validated_data.pop('tags', [])
+        seller = validated_data.pop('seller', None)
+        #seller, _ = get_object_or_404(StoreUser, pk=seller_id)
+        if not seller:
+            raise serializers.ValidationError(
+                {"seller_id": "Seller not registered or invalid ID."}
+            )
+        product = Product.objects.create(**validated_data, seller=seller)
+        for tag_data in tags:
+            tag_obj, _ = Tag.objects.get_or_create(**tag_data)
+            product.tags.add(tag_obj)
+
+        #product.save()
+        return product
+    
