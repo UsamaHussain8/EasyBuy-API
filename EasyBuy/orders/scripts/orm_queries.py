fd3c986dd76c9d4f288f92f71f70c33b61567c12
UsamaHussain8 <usamahussain00@gmail.com>
2025-11-18 23:42:59 +0500

    Solved problems related to filtering, Q objects and aggregations

--- EasyBuy/orders/scripts/orm_queries.py
+++ EasyBuy/orders/scripts/orm_queries.py
@@ -2,7 +2,7 @@ from django_extensions import db
 from core.models import StoreUser, User
 from products.models import Product
 from orders.models import Order, OrderItem, Cart, CartItem
-from django.db.models import Count, F
+from django.db.models import Count, F, Q, Sum
 from django.db import connection
 from pprint import pprint
 
@@ -36,6 +36,37 @@ def get_easypaisa_nayapay_payment_orders():
         print(order)
     pprint(connection.queries)
 
+def get_electronics_items_or_low_stock_items():
+    electronic_low_stock_items = Product.objects.filter(
+                                 Q(category__in=['electronics', 'Electronics']) | Q(quantity__lt=5)) \
+                                    .values('name', 'category', 'quantity', 'price')
+    for product in electronic_low_stock_items:
+        print(product)
+    pprint(connection.queries)    
+
+def get_customers_with_more_orders():
+    customers_with_one_or_more_orders = StoreUser.objects.filter(
+                                 Q(contact_number__endswith='7') | Q(num_orders__gt=1)) \
+                                 .annotate(username=F('user__username'), 
+                                           email=F('user__email')) \
+                                 .values('username', 'email', 'contact_number', 'num_orders')
+    for user in customers_with_one_or_more_orders:
+        print(user)
+    pprint(connection.queries)     
+
+def count_number_of_products():
+    num_products = Product.objects.count()
+    print(num_products)
+
+    pprint(connection.queries)
+
+def revenue_from_completed_orders():
+    total_revenue_completed_orders = Order.objects.filter(status="COMPLETED").aggregate(total_revenue=Sum('total_amount'))
+    print(total_revenue_completed_orders)
+
+    pprint(connection.queries)
+
+####################################################    
 def run(): 
     """
     Get all products with quantity less than 10
@@ -45,9 +76,29 @@ def run():
     """
     Get all buyers who have placed at least one order
     """
-    get_buyers_list()
+    # get_buyers_list()
 
     """
     Find all orders with payment method = EASYPAISA OR NAYAPAY
     """
     # get_easypaisa_nayapay_payment_orders()
+
+    """
+    Find products with category = electronics OR with low stock
+    """
+    # get_electronics_items_or_low_stock_items()
+
+    """
+    Find all buyers whose contact_number ends with '7' OR who have more than 1 order
+    """
+    # get_customers_with_more_orders()
+
+    """
+    Find total number of products in the database
+    """
+    # count_number_of_products()
+
+    """
+    Calculate total revenue from all COMPLETED orders
+    """
+    revenue_from_completed_orders()
